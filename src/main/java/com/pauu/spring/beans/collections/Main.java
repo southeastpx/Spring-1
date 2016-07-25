51e05a7e65aa85d6b6cebf0c443de0d96bbca9ff
peng.xing <southeast_px@163.com>
2016-07-25 14:57:45 +0800

    测试util schema and p schema

--- src/main/java/com/pauu/spring/beans/collections/Main.java
+++ src/main/java/com/pauu/spring/beans/collections/Main.java
@@ -6,11 +6,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
 	public static void main(String[] args) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-		Person person = (Person) ctx.getBean("person3");
+		/*Person person = (Person) ctx.getBean("person3");
 		System.out.println(person);
 		NewPerson newPerson = (NewPerson) ctx.getBean("newPerson");
 		System.out.println(newPerson);
 		DataSource dataSource = (DataSource) ctx.getBean("dataSource");
-		System.out.println(dataSource);
+		System.out.println(dataSource);*/
+		Person person = (Person) ctx.getBean("person4");
+		System.out.println(person);
+		person = (Person) ctx.getBean("person5");
+		System.out.println(person);
 	}
 }
