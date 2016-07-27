0bfce77dabef34ee501190896ce685d0668f1650
peng.xing <peng.xing@ZJ-OA-PENG-X.sfit.shfe.com.cn>
2016-07-27 15:10:17 +0800

    update file

---
+++ src/main/java/com/pauu/spring/beans/relation/Main.java
@@ -0,0 +1,20 @@
+package com.pauu.spring.beans.relation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.pauu.spring.beans.autowire.Address;
+import com.pauu.spring.beans.autowire.Person;
+
+public class Main {
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-relation.xml");
+		Address address = (Address) ctx.getBean("address3");
+		System.out.println(address);
+		address = (Address) ctx.getBean("address2");
+		System.out.println(address);
+		
+		Person person = (Person) ctx.getBean("person");
+		System.out.println(person);
+	}
+}
