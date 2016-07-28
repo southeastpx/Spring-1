ef2c43253696041894e1617b9b466914b882fb98
peng.xing <southeast_px@163.com>
2016-07-28 14:11:11 +0800

    spring bean生命周期管理

---
+++ src/main/java/com/pauu/spring/beans/cycle/Car.java
@@ -0,0 +1,22 @@
+package com.pauu.spring.beans.cycle;
+
+public class Car {
+	private String brand;
+	public Car() {
+		System.out.println("Car's Constructor...");
+	}
+	public void setBrand(String brand) {
+		System.out.println("setBrand...");
+		this.brand = brand;
+	}
+	public void init(){
+		System.out.println("init()...");
+	}
+	public void destroy(){
+		System.out.println("destroy()...");
+	}
+	@Override
+	public String toString() {
+		return "Car [brand=" + brand + "]";
+	}
+}
